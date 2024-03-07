68be24147771c013b8faf7062898ab50520a2c2a
ekaterinasavcuk <heeykat@gmail.com>
2024-03-07 20:27:14 +0100

    4.2. Заголовок в файле java [#159036 #98074] v.2

--- src/main/java/ru/job4j/Info.java
+++ src/main/java/ru/job4j/Info.java
@@ -1,6 +1,7 @@
 package ru.job4j;
+
 public class Info {
     public static void main(String[] args) {
-        System.out.println("06.08.2024");
+        System.out.println("06.03.2024");
     }
 }
