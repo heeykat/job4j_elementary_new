b5b8f6a1eb66f87a55896b7823b9fbc09ac829c0
ekaterinasavcuk <heeykat@gmail.com>
2024-05-28 12:26:20 +0200

    GPT. Нейросети спешат на помощь. [#505156]

--- src/main/java/ru/job4j/calculator/MathCalculator.java
+++ src/main/java/ru/job4j/calculator/MathCalculator.java
@@ -3,26 +3,43 @@ package ru.job4j.calculator;
 import static ru.job4j.math.MathFunction.*;
 
 public class MathCalculator {
-
-    public static double sumAndMultiply(double first, double second) {
-        return sum(first, second)
-                + multiply(first, second);
+    public static double sum(double first, double second) {
+        return first + second;
     }
 
-    public static double subAndDivision(double first, double second) {
-        return subtraction(first, second)
-                + division(first, second);
+    public static double multiply(double first, double second) {
+        return first * second;
+    }
+
+    public static double subtraction(double first, double second) {
+        return first - second;
+    }
+
+    public static double division(double first, double second) {
+        if (second == 0) {
+            throw new ArithmeticException("Division by zero is not allowed");
+        }
+        return first / second;
+    }
+
+    public static double sumAndMultiply(double first, double second) {
+        return sum(first, second) + multiply(first, second);
+    }
+
+    public static double subtractAndDivide(double first, double second) {
+        return subtraction(first, second) + division(first, second);
     }
 
     public static double allMathOperations(double first, double second) {
-        return sumAndMultiply(first, second)
-                + subAndDivision(first, second);
+        return sumAndMultiply(first, second) + subtractAndDivide(first, second);
     }
 
     public static void main(String[] args) {
-        System.out.println("Результат sumAndMultiply равен: " + sumAndMultiply(30, 15));
-        System.out.println("Результат subAndDivision равен: " + subAndDivision(30, 15));
-        System.out.println("Результат allMathOperations равен: " + allMathOperations(30, 15));
-    }
+        double first = 30;
+        double second = 15;
 
+        System.out.println("Результат sumAndMultiply равен: " + sumAndMultiply(first, second));
+        System.out.println("Результат subtractAndDivide равен: " + subtractAndDivide(first, second));
+        System.out.println("Результат allMathOperations равен: " + allMathOperations(first, second));
+    }
 }
